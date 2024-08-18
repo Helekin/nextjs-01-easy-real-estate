f4adb2b9d86c0d7a5ba9209508dae45bfde70031
Helekin <clsjs94@hotmail.com>
2024-08-18 16:45:22 -0500

    navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,19 @@
-const MainLayout = () => {
+import "@/assets/styles/globals.css";
+
+import Navbar from "@/components/Navbar";
+
+export const metadata = {
+  title: "Property Pulse",
+  keywords: "rental property, real estate",
+  description: "Find the perfect rental property",
+};
+
+const MainLayout = ({ children }) => {
   return (
     <html>
       <body>
-        <main>Main Layout</main>
+        <Navbar/>
+        <main>{children}</main>
       </body>
     </html>
   );
